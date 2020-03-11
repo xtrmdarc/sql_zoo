fbd8a0a91ffb0bb996c41a031c4c9ac85e8fbebc
xtrmdarc <diegoreyesco@hotmail.com>
2020-03-10 19:56:24 -0500

    Complete 8 task

--- sql_files/8+_nss_tutorial.sql
+++ sql_files/8+_nss_tutorial.sql
@@ -21,3 +21,27 @@ WHERE question='Q22'
 AND subject in ('(8) Computer Science','(H) Creative Arts and Design')
 group by subject;
 
+SELECT subject, sum(response * A_STRONGLY_AGREE/100)
+FROM nss
+WHERE question='Q22'
+AND subject in ('(8) Computer Science','(H) Creative Arts and Design')
+group by 1;
+
+SELECT subject, round(sum(response * A_STRONGLY_AGREE)/sum(response))
+FROM nss
+WHERE question='Q22'
+AND subject in ('(8) Computer Science','(H) Creative Arts and Design')
+group by 1;
+
+SELECT institution,  round(sum(response*score/100)/sum(response) * 100)
+FROM nss
+WHERE question='Q22'
+AND (institution LIKE '%Manchester%')
+group by 1
+ORDER BY institution;
+
+SELECT institution, sum(sample), sum(case when subject = '(8) Computer Science' then response else 0 end)
+  FROM nss
+ WHERE question='Q01'
+   AND (institution LIKE '%Manchester%')
+group by 1;
